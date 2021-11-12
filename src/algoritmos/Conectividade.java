3fbd1ffb1eeba23e1513939301f7aa2c7143b8f5
Marco Aurélio Silva de Souza Júnior <aureliojuniorcmrj@hotmail.com>
2021-11-12 16:55:59 -0300

    código traduzido para português

--- src/algoritmos/Conectividade.java
+++ src/algoritmos/Conectividade.java
@@ -1,3 +1,5 @@
+// https://algorithms.tutorialhorizon.com/check-if-given-undirected-graph-is-connected-or-not/
+
 package algoritmos;
 
 import java.util.LinkedList;
@@ -9,65 +11,67 @@ import util.GeraGrafo;
 
 public class Conectividade {
 
+  public static boolean ehConexo(Grafo grafo) {
+    boolean resp;
 
+    int numVertices = grafo.getNumeroVertices();
+    LinkedList<Vertice> vertices = grafo.getVertices();
 
-  public static void isConnected(Grafo graph) {
-
-    int numVertices = graph.getNumeroVertices();
-    LinkedList<Vertice> adjList = graph.getVertices();
-
-    // created visited array
+    // array que indica índices dos vértices visitados
     boolean[] visitados = new boolean[numVertices];
 
-    // start the DFS from vertex 0
-    Vertice zero = graph.getVertice(0);
-    DFS(zero, adjList, visitados);
+    // faz busca em profundidade a partir do vértice 0
+    Vertice zero = grafo.getVertice(0);
+    buscaProfundidade(zero, vertices, visitados);
 
-    // check if all the vertices are visited, if yes then graph is connected
+    // checa se todos os vértices foram visitados, se sim, então o grafo é conexo
     int count = 0;
     for (int i = 0; i < visitados.length; i++) {
       if (visitados[i])
         count++;
     }
     if (numVertices == count) {
-      System.out.println("Given graph is connected");
+      resp = true;
     } else {
-      System.out.println("Given graph is not connected");
+      resp = false;
     }
+    return resp;
   }
 
-  public static void DFS(Vertice verticeOrigem, LinkedList<Vertice> adjList, boolean[] visitados) {
-    int indiceVerticeOrigem = verticeOrigem.getIndice() - 1;
-    // mark the vertex visited
+
+  public static void buscaProfundidade(Vertice verticeOrigem, LinkedList<Vertice> vertices, boolean[] visitados) {
+    int indiceVerticeOrigem = verticeOrigem.getIndice();
+    // marca o vértice como visitado
     visitados[indiceVerticeOrigem] = true;
 
-    // travel the neighbors
+    // iteração sobre os adjacentes
     for (int i = 0; i < verticeOrigem.numeroArestas(); i++) {
-      Vertice neighbor = verticeOrigem.getAdjacentes().get(i);
-      if (visitados[neighbor.getIndice()-1] == false) {
-        // make recursive call from neighbor
-        DFS(neighbor, adjList, visitados);
+      Vertice adjacente = verticeOrigem.getAdjacente(i);
+      if (visitados[adjacente.getIndice()] == false) {
+        // faz chamada recursiva a partir do adjacente
+        buscaProfundidade(adjacente, vertices, visitados);
       }
     }
   }
 
   public static void main(String[] args) {
-    /** * /
-    Grafo graph = new Grafo();
+    /** */
+    Grafo grafo = new Grafo();
 
     
-    graph.cria_adjacencia("0", "1");
-    graph.cria_adjacencia("1", "3");
-    graph.cria_adjacencia("3", "2");
-    graph.criaVertice("4");
+    grafo.cria_adjacencia("0", "1");
+    grafo.cria_adjacencia("1", "3");
+    grafo.cria_adjacencia("3", "2");
+    grafo.criaVertice("4");
 
-    Conectividade c = new Conectividade();
-    c.isConnected(graph);
-
-    graph.cria_adjacencia("3", "4");
-    c.isConnected(graph);
-    /** */
+    System.out.println(Conectividade.ehConexo(grafo)? "Grafo é conexo" : "Grafo não é conexo" );    
+    
+    grafo.cria_adjacencia("3", "4");
+    System.out.println(Conectividade.ehConexo(grafo)? "Grafo é conexo" : "Grafo não é conexo" );
+    
+    /** * /
     Grafo grafo = GeraGrafo.fromPajek("src/dados/grafoSalvo.pajek");
-    Conectividade.isConnected(grafo);
+    System.out.println(Conectividade.ehConexo(grafo)? "Grafo é conexo" : "Grafo não é conexo" );
+    /** */
   }
 }
